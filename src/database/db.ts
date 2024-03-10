2be965608ca44ec2dcd78da4d13e73d081d325ec
almela09 <145797671+almela09@users.noreply.github.com>
2024-03-10 09:58:43 +0100

    basic model

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,14 +4,7 @@ import "reflect-metadata";
 import "dotenv/config";
 
 import { DataSource } from "typeorm";
-import { Roles1709670304552 } from "./migrations/1709670304552-roles";
-import { Users1709671000479 } from "./migrations/1709671000479-users";
-import { Services1709671010055 } from "./migrations/1709671010055-services";
-import { Appointments1709671019629 } from "./migrations/1709671019629-appointments";
-import { Role } from "../models/Role";
-import { User } from "../models/User";
-import { Service } from "../models/Service";
-import { Appointment } from "../models/Appointments";
+
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -20,12 +13,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_DATABASE || "",
-  entities: [Role, User, Service, Appointment],
+  entities: [],
   migrations: [
-    Roles1709670304552,
-    Users1709671000479,
-    Services1709671010055,
-    Appointments1709671019629,
+   
   ],
   synchronize: false,
   logging: false,
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
 import express, { Application } from "express"; // desde que librería? express.
 import dotenv from "dotenv";
-import {
-  createRole,
-  deleteRole,
-  getRoles,
-  updateRole,
-} from "./controllers/roleController";
+
 import { AppDataSource } from "./database/db";
-import { getUsers } from "./controllers/userController";
 
 dotenv.config();
 
@@ -23,18 +17,21 @@ app.get("/api/healthy", (req, res) => {
 });
 
 //Roles routes -endpoint.
-app.get("/api/roles", getRoles); //lo modificamos en roleControllers.
-app.post("/api/roles", createRole);
-app.put("/api/roles/:id", updateRole); //cambiar informacion de todas las columnas /// PARAMETRO DE RUTA :id, significa que este dato es dinámico.
-app.delete("/api/roles/:id", deleteRole);
+app.get("/api/roles"); //lo modificamos en roleControllers.
+app.post("/api/roles");
+app.put("/api/roles/:id"); //cambiar informacion de todas las columnas /// PARAMETRO DE RUTA :id, significa que este dato es dinámico.
+app.delete("/api/roles/:id");
 
-//AUTH routes
-// app.post('/api/auth/register', register)
 
 // User routes
 app.get("/api/users");
 app.get("/api/users/profile");
 app.put("api/users/profile");
+
+
+//Services routes
+
+
 
 //Appointments
 
@@ -42,13 +39,7 @@ app.post("api/appointments");
 app.put("/api/appointments");
 app.get("/api/appointments/{id}");
 app.get("/api/appointments");
-// app.get('/api/users/:id', getUserById)
-// app.put('/api/users/:id', updateUserById)
-// app.delete('/api/users/:id', deleteUserById)
 
-//services routes
-
-//appointments routes
 
 //la app tiene que escuchar---- RUTAS. Por ejemplo si no la tienes que mandas? un 404 o no existe.
 //Estructura básica para el servidor
